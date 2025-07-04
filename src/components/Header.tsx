6d38587d85176bb93c4aa16b3aa868a731432e59
CaiorFeliciano <caiorfeliciano@gmail.com>
2025-07-04 18:44:40 -0300

    feat: Debouce

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -9,6 +9,7 @@ import Box from "@mui/material/Box";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
 import MenuItem from "@mui/material/MenuItem";
+import { useDebounce } from "../hooks/useDebounce";
 
 interface HeaderProps {
   favoritosCount: number;
@@ -26,69 +27,84 @@ const Header: React.FC<HeaderProps> = ({
   categorieFilter,
   onNameFilterChange,
   onCategorieFilterChange,
-}) => (
-  <AppBar position="fixed">
-    <Toolbar>
-      <Typography variant="h6" sx={{ flexGrow: 1, color: "#fff" }}>
-        Smartbreeder Produtos
-      </Typography>
-      <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-        <TextField
-          id="standart"
-          size="small"
-          variant="standard"
-          placeholder="Buscar por nome"
-          value={nameFilter}
-          onChange={(e) => onNameFilterChange(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon sx={{ color: "#fff" }} />
-              </InputAdornment>
-            ),
-            disableUnderline: false,
-            sx: { color: "#fff" },
-          }}
-          InputLabelProps={{
-            sx: { color: "#fff" },
-          }}
-          sx={{
-            input: { color: "#fff" },
-            "& .MuiInput-underline:before": { borderBottomColor: "#fff" },
-            "& .MuiInput-underline:after": { borderBottomColor: "#fff" },
-          }}
-        />
-        <TextField
-          size="small"
-          select
-          variant="outlined"
-          label="Categoria"
-          value={categorieFilter}
-          onChange={(e) => onCategorieFilterChange(e.target.value)}
-          sx={{
-            minWidth: 120,
-            "& .MuiInputBase-input": { color: "#fff" },
-            "& .MuiOutlinedInput-notchedOutline": { borderColor: "#fff" },
-            "& .MuiSvgIcon-root": { color: "#fff" },
-            "& .MuiInputLabel-root": { color: "#fff" },
-          }}
-          InputLabelProps={{
-            sx: { color: "#fff" },
-          }}
-        >
-          <MenuItem value="">Todas</MenuItem>
-          {categories.map((cat) => (
-            <MenuItem key={cat.id} value={cat.id}>
-              {cat.nome}
-            </MenuItem>
-          ))}
-        </TextField>
-        <Badge badgeContent={favoritosCount} color="error">
-          <FavoriteIcon sx={{ color: "#fff" }} />
-        </Badge>
-      </Box>
-    </Toolbar>
-  </AppBar>
-);
+}) => {
+  const [localNameFilter, setLocalNameFilter] = useState(nameFilter);
+  const debouncedNameFilter = useDebounce(localNameFilter, 500);
+
+  useEffect(() => {
+    setLocalNameFilter(nameFilter);
+  }, [nameFilter]);
+
+  useEffect(() => {
+    if (debouncedNameFilter !== nameFilter) {
+      onNameFilterChange(debouncedNameFilter);
+    }
+  }, [debouncedNameFilter, nameFilter, onNameFilterChange]);
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1, color: "#fff" }}>
+          Smartbreeder Produtos
+        </Typography>
+        <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+          <TextField
+            id="standart"
+            size="small"
+            variant="standard"
+            placeholder="Buscar por nome"
+            value={localNameFilter}
+            onChange={(e) => setLocalNameFilter(e.target.value)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon sx={{ color: "#fff" }} />
+                </InputAdornment>
+              ),
+              disableUnderline: false,
+              sx: { color: "#fff" },
+            }}
+            InputLabelProps={{
+              sx: { color: "#fff" },
+            }}
+            sx={{
+              input: { color: "#fff" },
+              "& .MuiInput-underline:before": { borderBottomColor: "#fff" },
+              "& .MuiInput-underline:after": { borderBottomColor: "#fff" },
+            }}
+          />
+          <TextField
+            size="small"
+            select
+            variant="outlined"
+            label="Categoria"
+            value={categorieFilter}
+            onChange={(e) => onCategorieFilterChange(e.target.value)}
+            sx={{
+              minWidth: 120,
+              "& .MuiInputBase-input": { color: "#fff" },
+              "& .MuiOutlinedInput-notchedOutline": { borderColor: "#fff" },
+              "& .MuiSvgIcon-root": { color: "#fff" },
+              "& .MuiInputLabel-root": { color: "#fff" },
+            }}
+            InputLabelProps={{
+              sx: { color: "#fff" },
+            }}
+          >
+            <MenuItem value="">Todas</MenuItem>
+            {categories.map((cat) => (
+              <MenuItem key={cat.id} value={cat.id}>
+                {cat.nome}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Badge badgeContent={favoritosCount} color="error">
+            <FavoriteIcon sx={{ color: "#fff" }} />
+          </Badge>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
